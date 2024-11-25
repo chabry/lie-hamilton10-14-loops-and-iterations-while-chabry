d68d55397ebebc3259ff0f8f77441b234474f9b3
Bryan Charles <chabry01@gmail.com>
2024-11-25 15:33:29 +0100

    Exercice résolu

--- 06-03-loops-and-iterations-while-statement.js
+++ 06-03-loops-and-iterations-while-statement.js
@@ -2,22 +2,57 @@
 
 // Function 1
 function sumOfSquares(n) {
-  // Your code here
+  // Implement a function to calculate the sum of squares of numbers from 1 to 'n'.   
+  let i = 1
+  let sum = 0
+
+  while(i <= n){
+    sum += Math.pow(i, 2)
+    i++
+  }
+  return sum
 }
 
 // Function 2
 function printReverseArray(arr) {
   // Your code here
+  let i = arr.length - 1
+
+  while(i >= 0){
+    console.log(arr[i])
+    i--
+  }
 }
 
 // Function 3
 function reverseString(str) {
-  // Your code here
+  //Implement a function to reverse the given string.
+  let reverseString = ''
+  let i = str.length - 1
+
+  while(i >= 0){
+    reverseString += str[i]
+    i--
+  }
+
+  return reverseString
 }
 
 // Function 4
 function findMax(arr) {
-  // Your code here
+  //Implement a function to find the maximum number in the given array.
+  let maxNumber = arr[0]
+  let i = 0
+
+  while(i < arr.length){
+    if(arr[i] > maxNumber){
+      maxNumber = arr[i]
+    }
+    i++
+  }
+
+  return maxNumber
+
 }
 
 module.exports = {
